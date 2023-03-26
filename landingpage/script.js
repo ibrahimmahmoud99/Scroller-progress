30ace8ca6c1a1b4d840eb8382aaca50f95503b22
ibrahimmahmoud99 <71046431+ibrahimmahmoud99@users.noreply.github.com>
2023-03-26 09:41:35 +0300

    Add files via upload

--- landingpage/script.js
+++ landingpage/script.js
@@ -9,3 +9,23 @@ window.addEventListener("scroll", () => {
   const scrollTop = document.documentElement.scrollTop;
   el.style.width = `${(scrollTop / height) * 100}%`;
 });
+
+let btn = document.querySelector("button");
+
+window.onscroll = function () {
+  if (window.scrollY >= 50) {
+    console.log(`Scrolling Y Value Is ${window.scrollY}`);
+    btn.style.display = "block";
+  }
+  else {
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
